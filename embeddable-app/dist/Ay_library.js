99fdf3e03929c834ac18666ba47d7b66968bf615
aycom366 <bamigboyeayomide200@gmail.com>
2023-10-04 17:07:07 +0100

    setup for embeddable app completed

---
+++ embeddable-app/dist/Ay_library.js
@@ -0,0 +1,18 @@
+!(function (e, n) {
+  "object" == typeof exports && "undefined" != typeof module
+    ? n(exports)
+    : "function" == typeof define && define.amd
+    ? define(["exports"], n)
+    : n(
+        ((e =
+          "undefined" != typeof globalThis
+            ? globalThis
+            : e || self).Ay_library = {})
+      );
+})(this, function (e) {
+  "use strict";
+  e.add = function (e, n) {
+    return e + n;
+  };
+});
+//# sourceMappingURL=Ay_library.js.map
